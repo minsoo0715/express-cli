0a83de2d26e6d213bdb76a59c49148bcf40ea7ac
WhiteB <whiteblock0715@gmail.com>
2021-05-12 13:35:18 +0900

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,17 +1,2 @@
 #!/usr/bin/env node
-declare const chalk: any;
-declare const clear: any;
-declare const figlet: any;
-declare const path: any;
-declare const program: any;
-declare const shell: any;
-declare let args: Array<string>;
-interface temp_interface {
-    [key: string]: string;
-}
-declare const options: any;
-declare const temp: temp_interface;
-declare let temp_name: string;
-declare let set_name: string;
-declare let url: string;
-declare const cmd: string;
+export {};
